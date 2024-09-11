b92e188912280583724a5adefab90c31dde1f512
mitchellkolb <mkolb321@gmail.com>
2024-09-10 17:45:55 -0700

    Learned how to make the box clickable and cycle through demo text.

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
-import sys
-import gi
+import random
+from guiFile import RandomTextWindow, start_gui  # Import the correct names from guiFile
 
-gi.require_version("Gtk", "4.0")
-from gi.repository import GLib, Gtk
+# Define a list of random text strings
+random_text_list = [
+    "Hello, World!",
+    "Welcome to PyGObject!",
+    "Random Text Here",
+    "Enjoy coding in Python!",
+    "Linux Mint is awesome!"
+]
 
-class MyApplication(Gtk.Application):
-    def __init__(self):
-        super().__init__(application_id="com.example.MyGtkApplication")
-        GLib.set_application_name('My Gtk Application')
+def get_random_text():
+    """Return a random text from the list."""
+    return random.choice(random_text_list)
 
-    def do_activate(self):
-        window = Gtk.ApplicationWindow(application=self, title="Hello World")
+if __name__ == "__main__":
+    # Pass the random text function to the GUI and start it
+    start_gui(get_random_text)  # Use start_gui from guiFile
+
+
+
+# import sys
+# import gi
+
+# gi.require_version("Gtk", "4.0")
+# from gi.repository import GLib, Gtk
+
+# class MyApplication(Gtk.Application):
+#     def __init__(self):
+#         super().__init__(application_id="com.example.MyGtkApplication")
+#         GLib.set_application_name('My Gtk Application')
+
+#     def do_activate(self):
+#         window = Gtk.ApplicationWindow(application=self, title="Hello World")
         
-        # Setting the default window size (width x height) cause standard is too small
-        window.set_default_size(450, 500)
+#         # Setting the default window size (width x height) cause standard is too small
+#         window.set_default_size(450, 500)
         
-        window.present()
+#         window.present()
 
-app = MyApplication()
-exit_status = app.run(sys.argv)
-sys.exit(exit_status)
+# app = MyApplication()
+# exit_status = app.run(sys.argv)
+# sys.exit(exit_status)
